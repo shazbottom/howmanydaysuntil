3ef0ed46e13e906e826e540b54efba8d9c6bdea8
shazbot <Paul.Rose@gmail.com>
2026-03-29 15:18:06 +1100

    Remove looping www redirect rule

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,6 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.daysuntil.is",
-          },
-        ],
-        destination: "https://daysuntil.is/:path*",
-        permanent: true,
-      },
-      {
         source: "/days-until/:slug",
         destination: "/days-until-:slug",
         permanent: true,
